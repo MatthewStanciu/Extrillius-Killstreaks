93c309c4fad6cd063244f9a6ff51fd40f9f6d5a8
TechBug2012 <mattbstanciu@gmail.com>
2016-04-26 22:11:08 -0400

    init

---
+++ src/Killstreaks.java
@@ -0,0 +1,23 @@
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by TechBug2012 on 4/26/16.
+ */
+public class Killstreaks extends JavaPlugin implements Listener {
+
+    public void onEnable() {
+
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onKill(PlayerDeathEvent event) {
+        String killed = event.getEntity().getDisplayName();
+        String killer = event.getEntity().getKiller().getDisplayName();
+
+
+    }
+}
