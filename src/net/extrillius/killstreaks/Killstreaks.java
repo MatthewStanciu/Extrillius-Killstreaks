669d4b758e45f75090bb23e98dd6c6a458e92024
TechBug2012 <mattbstanciu@gmail.com>
2016-04-28 07:26:05 -0400

    Merge branch 'master' of /Users/mattbstanciu/Extrillius-Killstreaks with conflicts.

--- src/net/extrillius/killstreaks/Killstreaks.java
+++ src/net/extrillius/killstreaks/Killstreaks.java
@@ -1,116 +1,40 @@
 package net.extrillius.killstreaks;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
  * Created by TechBug2012 on 4/26/16.
  */
 public class Killstreaks extends JavaPlugin implements Listener {
-    private HashMap<Integer, String> kills = new HashMap<>();
-    int killsNumber = 1;
-    int id;
-    boolean timer;
-    Entity hit; // watch out for npe
-    Entity hitter; // watch out for npe
-    String hitName;
-    String hitterName;
-    Player killed;
-    Player killer;
-    String killedName;
-    String killerName;
+    HashMap<Integer, String> kills = new HashMap<Integer, String>();
 
     public void onEnable() {
 
         getServer().getPluginManager().registerEvents(this, this);
     }
-    @EventHandler
-    public void onHit(EntityDamageByEntityEvent event) {
-        timer = true;
-        if (hit instanceof Player && hitter instanceof Player) {
-            hit = event.getEntity();
-            hitter = event.getDamager();
-            hitName = ((Player) hit).getDisplayName();
-            hitterName = ((Player) hitter).getDisplayName();
-        }
-        if (id != 0) {
-            getServer().getScheduler().cancelTask(id);
-        }
 
-        this.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
-            @Override
-            public void run() {
-                timer = false;
-            }
-        }, 200L);
-
-    }
     @EventHandler
     public void onKill(PlayerDeathEvent event) {
-        if (killed != null && killer != null) {
-            killedName = event.getEntity().getDisplayName();
-            killerName = event.getEntity().getKiller().getDisplayName();
-            killed = event.getEntity();
-            killer = event.getEntity().getKiller();
-        }
-        if (hit != null && hit.getLastDamageCause() != null) {
-            if (hit.getLastDamageCause().getCause() == EntityDamageEvent.DamageCause.VOID) {
-                if (timer) {
-                    getServer().getScheduler().cancelTask(id);
-                    event.setDeathMessage(ChatColor.AQUA + killedName + ChatColor.GRAY
-                            + " was thrown into the void by " + ChatColor.AQUA + killerName);
-                } else {
-                    event.setDeathMessage(ChatColor.AQUA + killedName + ChatColor.GRAY + " fell into the void");
-                }
-            } else if (hit.getLastDamageCause().getCause() == EntityDamageEvent.DamageCause.FALL) {
-                if (timer) {
-                    getServer().getScheduler().cancelTask(id);
-                    event.setDeathMessage(ChatColor.AQUA + killedName + ChatColor.GRAY
-                            + " was thrown off a cliff by " + ChatColor.AQUA + killerName);
-                } else {
-                    event.setDeathMessage(ChatColor.AQUA + killedName + ChatColor.GRAY + " fell off a cliff");
-                }
-            } else if (hit.getLastDamageCause().getCause() == EntityDamageEvent.DamageCause.LAVA) {
-                if (timer) {
-                    getServer().getScheduler().cancelTask(id);
-                    event.setDeathMessage(ChatColor.AQUA + killedName + ChatColor.GRAY
-                            + " was thrown into a pit of of lava by " + ChatColor.AQUA + killerName);
-                } else {
-                    event.setDeathMessage(ChatColor.AQUA + killedName + ChatColor.GRAY + " jumped into lava");
-                }
-            } else if (hit.getLastDamageCause().getCause() == EntityDamageEvent.DamageCause.FIRE) {
-                if (timer) {
-                    getServer().getScheduler().cancelTask(id);
-                    event.setDeathMessage(ChatColor.AQUA + killedName + ChatColor.GRAY
-                            + " was burned by " + ChatColor.AQUA + killerName);
-                } else {
-                    event.setDeathMessage(ChatColor.AQUA + killedName + ChatColor.GRAY + " burned to death");
-                }
-            } else if (hit.getLastDamageCause().getCause() == EntityDamageEvent.DamageCause.PROJECTILE) {
-                if (timer) {
-                    getServer().getScheduler().cancelTask(id);
-                    event.setDeathMessage(ChatColor.AQUA + killedName + ChatColor.GRAY
-                            + " was shot by " + ChatColor.AQUA + killerName);
-                } else {
-                    event.setDeathMessage(ChatColor.AQUA + killedName + ChatColor.GRAY + " shot themselves");
-                }
-            } else if (hit.getLastDamageCause().getCause() == EntityDamageEvent.DamageCause.SUICIDE) {
-                event.setDeathMessage(ChatColor.AQUA + killedName + ChatColor.GRAY + " killed themselves.");
-            } else {
-                event.setDeathMessage(ChatColor.AQUA + killedName + ChatColor.GRAY + " was killed by " + ChatColor.AQUA
-                        + killerName);
-            }
+        String killedName = event.getEntity().getDisplayName();
+        String killerName = event.getEntity().getKiller().getDisplayName();
+        Player killed = event.getEntity();
+        Player killer = event.getEntity().getKiller();
+        int killsNumber = 1;
+
+        event.setDeathMessage(ChatColor.AQUA + killedName + ChatColor.GRAY + "was killed by " + ChatColor.AQUA + killerName);
+        if (killed.getLocation().getY() < 10) {
+            killed.setHealthScale(0);
+            event.setDeathMessage(ChatColor.AQUA + killedName + ChatColor.GRAY + "fell into the void");
         }
 
         if (!(kills.containsValue(killerName))) {
@@ -118,45 +42,33 @@ public class Killstreaks extends JavaPlugin implements Listener {
         }
         if (kills.containsValue(killerName)) {
             kills.remove(killsNumber);
-            kills.remove(killerName); // stupid warning here
-            killsNumber++;
+            kills.remove(killerName); // stupid error here
+            killsNumber++; 
             kills.put(killsNumber, killerName);
         }
-        if (killer.isDead()) {
+        if (killer.getHealth() == 0) {
             kills.remove(killsNumber);
-            kills.remove(killerName); // another stupid warning here
-            getServer().broadcastMessage(ChatColor.AQUA + killerName + ChatColor.GREEN + " was shutdown with "
-                    + ChatColor.RED + kills.get(killsNumber) + ChatColor.GREEN + " kills!");
-        }
-        if (kills.get(killsNumber).equals("5")) {
-            getServer().broadcastMessage(ChatColor.AQUA + killerName + ChatColor.GREEN + " is on a killstreak!");
-        }
-        if (kills.get(killsNumber).equals("10")) {
-            getServer().broadcastMessage(ChatColor.AQUA + killerName + ChatColor.RED + " is on a rampage!");
-        }
-        if (kills.get(killsNumber).equals("20")) {
-            getServer().broadcastMessage(ChatColor.AQUA + killerName + ChatColor.YELLOW + " is " +
-                    ChatColor.UNDERLINE + ChatColor.RED + "UNSTOPPABLE!");
+            kills.remove(killerName);
+            // tell the server
         }
     }
+    @EventHandler
+    public void onHit(EntityDamageByEntityEvent event) {
+        Player hit = (Player) event.getEntity();
+        Player hitter = (Player) event.getDamager();
+        String hitName = hit.getDisplayName();
+        String hitterName = hitter.getDisplayName();
 
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (cmd.getName().equalsIgnoreCase("kills")) {
-            if (kills.containsValue(sender.getName())) {
-                if (!(kills.get(killsNumber)).equals("1")) {
-                    sender.sendMessage(ChatColor.GRAY + "You have " + ChatColor.GREEN + kills.get(killsNumber)
-                            + ChatColor.GRAY + " kills");
+        this.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+            @Override
+            public void run() {
+                /*
+                if (hit.getHealth() == 0) {
+                    I don't know how to do this
                 }
-                else {
-                    sender.sendMessage(ChatColor.GRAY + "You have " + ChatColor.GREEN + kills.get(killsNumber)
-                            + ChatColor.GRAY + " kill");
-                }
+                */
             }
-            else {
-                sender.sendMessage(ChatColor.GRAY + "You don't have any kills yet.");
-            }
-        }
+        }, 200L);
 
-        return true;
     }
 }
